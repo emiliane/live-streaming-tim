bae1cbffed3ad48f5ffd0231399c1c2c414fe0d2
emiliane <emiliane@users.noreply.github.com>
2016-06-01 02:17:41 +0300

    More complex.

--- python/list/create.py
+++ python/list/create.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 file = open('list.txt','r')
 
-i = 0
 with file as f:
+    i = 0
+    prevline = ""
+
     for line in f:
+        line = line.strip()
         channelNumber = str(int(i / 2) + 1)
+
         if i % 2 == 0:
-            print ("channel" + channelNumber + "=" + line   , end = '')
+            diff = ""
         else:
-            print ("channeladress" + channelNumber + "=" + line   , end = '')
+            diff = "adress"
+
+        fine = "channel" + diff + channelNumber + "=" + line
+        print(fine)
+
         i = i + 1
+
+        isDVR = line.strip()[-4:].lower()
+        if isDVR == "?dvr":
+            channelNumber = str(int(i / 2) + 1)
+            i = i + 2
+
+            fine1 = "channel" + channelNumber + "=" + prevline + " 2"
+            fine2 = "channeladress" + channelNumber + "=" + line[:-4]
+            print (fine1)
+            print (fine2)
+
+        prevline = line
